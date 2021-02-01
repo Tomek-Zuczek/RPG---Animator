367bd759b75b218e9f55b0771e0fcedf17c0d9b8
Tomek-Zak <zaktomek14@gmail.com>
2021-02-02 00:04:59 +0100

    no message

--- Assets/Skills.cs
+++ Assets/Skills.cs
@@ -1,21 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class Movement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     Animator animator;
     [SerializeField] GameObject[] skillParticles;
     private IEnumerator coroutine;
-    [SerializeField] Slider hpBar, manaBar;
 
     void Start()
     {
         animator = GetComponent<Animator>();
-
-        hpBar.value = hpBar.maxValue;
-        manaBar.value = manaBar.maxValue;
 
         for (int i = 0; i < skillParticles.Length; i++)
         {
@@ -42,12 +37,12 @@ public class Movement : MonoBehaviour
 
         if (Input.GetKeyUp(KeyCode.Alpha1)) UseSkill(1);
         if (Input.GetKeyUp(KeyCode.Alpha2)) UseSkill(2);
-
+        
     }
 
+    
 
-
-    public void UseSkill(int skillNumber)
+    void UseSkill(int skillNumber)
     {
         animator.SetTrigger("Skill" + skillNumber);
         skillParticles[skillNumber - 1].SetActive(true);
@@ -55,13 +50,12 @@ public class Movement : MonoBehaviour
         switch (skillNumber)
         {
             case 1:
-                gameObject.GetComponent<AudioSource>().Play();
                 coroutine = WaitToEnableObject(skillParticles[skillNumber - 1], 5f);
                 break;
             case 2:
                 coroutine = WaitToEnableObject(skillParticles[skillNumber - 1], 2f);
                 break;
-
+            
         }
 
         StartCoroutine(coroutine);
@@ -78,5 +72,4 @@ public class Movement : MonoBehaviour
     {
         animator.SetTrigger("Jump");
     }
-
 }
---
+++ Assets/UIController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject panel;
+    void Start()
+    {
+        panel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+            panel.SetActive(true);
+
+        if (Input.GetKeyUp(KeyCode.T))
+            panel.SetActive(false);
+
+
+    }
+}
